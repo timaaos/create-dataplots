ef3bd33a9f30b59001b0ba40e0a4439aa7243381
Тимофей Сухов <tsukhov77@gmail.com>
2024-05-06 18:13:34 +0300

    Dashboard recipe fix. Added Dashboard to Create's creative tab.

--- src/main/java/ru/timaaos/CreateDataAndPlots.java
+++ src/main/java/ru/timaaos/CreateDataAndPlots.java
@@ -1,5 +1,6 @@
 package ru.timaaos;
 
+import com.simibubi.create.AllCreativeModeTabs;
 import com.simibubi.create.content.kinetics.BlockStressDefaults;
 import com.simibubi.create.content.redstone.displayLink.AllDisplayBehaviours;
 import com.simibubi.create.foundation.data.AssetLookup;
@@ -10,11 +11,13 @@ import com.tterrag.registrate.util.entry.BlockEntry;
 import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
 import net.minecraft.block.Block;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -34,11 +37,16 @@ public class CreateDataAndPlots implements ModInitializer {
 			new Identifier(ID, "dashboard"),
 			FabricBlockEntityTypeBuilder.create(DashboardBlockEntity::new, DASHBOARD_BLOCK).build()
 	);
+	public static final Item DASHBOARD_BLOCK_ITEM = new BlockItem(DASHBOARD_BLOCK, new FabricItemSettings());
 	@Override
 	public void onInitialize() {
 		Registry.register(Registries.BLOCK, new Identifier(ID, "dashboard"), DASHBOARD_BLOCK);
-		Registry.register(Registries.ITEM, new Identifier(ID, "dashboard"), new BlockItem(DASHBOARD_BLOCK, new FabricItemSettings()));
+		Registry.register(Registries.ITEM, new Identifier(ID, "dashboard"), DASHBOARD_BLOCK_ITEM);
 		AllDisplayBehaviours.assignDataBehaviour(new DashboardBlockTarget(), "dashboard").accept(DASHBOARD_BLOCK);
+		ItemGroupEvents.modifyEntriesEvent(AllCreativeModeTabs.BASE_CREATIVE_TAB.key()).register(content -> {
+			content.add(DASHBOARD_BLOCK_ITEM);
+		});
+
 		LOGGER.info("Hello from Create: Data & Plots!");
 	}
 }
